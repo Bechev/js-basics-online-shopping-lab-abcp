543883ca80de6bb1b9f5e0553745abf18d1e17d7
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 11:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,42 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ var itemPrice = Math.floor(Math.random()*100);
+ var item = {[itemName]: itemPrice};
+ cart.push(item);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  var cartList = "In your cart, you have "
+  if(cart.length === 0){
+    cartList = "Your shopping cart is empty"
+  }
+  cart.forEach(function(element,index){
+    if(index === 0){
+      cartList= cartList + `${Object.keys(cart[index])} at $${Object.values(cart[index])}`;
+    }else if(cart.length === 2){
+        cartList= cartList + ` and ${Object.keys(cart[index])} at $${Object.values(cart[index])}`;
+    } else if(index != cart.length -1 ){
+      cartList= cartList + `, ${Object.keys(cart[index])} at $${Object.values(cart[index])}`;
+    }else {
+      cartList= cartList + `, and ${Object.keys(cart[index])} at $${Object.values(cart[index])}`;
+    }
+  })
+  console.log(cartList+".");
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  cart.forEach(function(element,index){
+    total = total + parseInt(Object.values(cart[index]));
+  })
+  return total;
 }
 
 function removeFromCart(item) {
