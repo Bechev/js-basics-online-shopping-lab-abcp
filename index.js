99f986f1408fcf02c5e8b0f1f1bbca07af52d4af
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 11:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  if(cart.hasOwnProperty(item) == true){
+  if(cart.hasOwnProperty(item) === false){
     delete cart[item];
     return cart;
   } else {
